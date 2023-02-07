81411010f23f4cfa9906225faeb2656338c2cc51
Jrpbjr <jrpbjr@gmail.com>
2023-02-07 20:30:39 -0300

    inicial commit

---
+++ src/test/java/br/com/inosystem/nfe/NfeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inosystem.nfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
